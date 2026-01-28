eaf5263736b914dd2565f39e0a330f07ccd436a4
Xkonti <xkonti@xkonti.tech>
2026-01-28 11:25:33 -0500

    Fix linting

--- src/scheduling/scheduling_service_test.ts
+++ src/scheduling/scheduling_service_test.ts
@@ -1,5 +1,4 @@
 import { expect } from "@std/expect";
-import { RecordId } from "surrealdb";
 import { TestSetupBuilder } from "../test/test_setup_builder.ts";
 import { integrationTest } from "../test/test_helpers.ts";
 import { JobProcessorService } from "../jobs/job_processor_service.ts";
