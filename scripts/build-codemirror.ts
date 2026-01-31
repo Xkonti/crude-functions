424fc39d11670f890fe75e41a3d5ea33b265a700
Xkonti <xkonti@xkonti.tech>
2026-01-31 01:51:03 -0500

    Fix linter issues

--- scripts/build-codemirror.ts
+++ scripts/build-codemirror.ts
@@ -5,8 +5,8 @@
  * Run with: deno run -A scripts/build-codemirror.ts
  */
 
-import * as esbuild from "npm:esbuild@0.24.0";
-import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@0.11.0";
+import * as esbuild from "esbuild";
+import { denoPlugins } from "@luca/esbuild-deno-loader";
 
 // Load completions data
 const completions = JSON.parse(
